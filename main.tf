b548d2d8765337445d90416e046b997b943deeb3
test <fanor.quintero@davivienda.com>
2025-05-16 15:39:59 -0500

    feat: se sube base

---
+++ main.tf
@@ -0,0 +1,16 @@
+/**
+ * This Terraform configuration creates a private Route53 zone in AWS.
+ * It requires the AWS provider and a VPC to be created beforehand.
+ */
+resource "aws_route53_zone" "private_zone" {
+  name = var.zone_id
+  vpc {
+    vpc_id = var.vpc_id
+  }
+  comment       = "Private Route53 zone"
+  force_destroy = false
+
+  depends_on = [ var.vpc_id ]
+
+  tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+# Global variables
+variable "tags" {
+	description = "Tags to assign to the resources"
+	type        = map(string)
+	default     = {
+		Manager     = "Terraform"
+		Environment = "dev"
+	}
+}
+
+# Route53 variables
+variable "zone_id" {
+	description = "The ID of the Route53 zone"
+	type = string
+}
+
+variable "vpc_id" {
+	description = "The ID of the VPC"
+	type = string
+}
+
